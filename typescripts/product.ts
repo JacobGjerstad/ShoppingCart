671b3bf204165fc2cd306ec0121e64222eaec87b
JACOB Gjerstad <gjerstad0715@students.cptc.edu>
2019-11-12 13:46:49 -0800

    Created a Product class

---
+++ typescripts/product.ts
@@ -0,0 +1,19 @@
+/**
+ * Represents a single shopping cart item
+ */
+class Product{
+    /**
+     * Name of product
+     */
+    title: string;
+    description: string;
+    price: Number;
+}
+
+// Test Code
+/*
+let prod = new Product();
+prod.title = "something";
+prod.description = "something else";
+prod.price = 4.99;
+*/
