6f7b9757a678d1d10168919f3066d16d5875346d
Jacob Gjerstad <jgjerstad@comcast.net>
2019-11-12 14:40:51 -0800

    Retrieving product is functional

--- typescripts/main.ts
+++ typescripts/main.ts
@@ -14,5 +14,25 @@ function initBuyButtons() {
 }
 
 function buyProduct(){
-    alert("You clicked buy");
+    let currBuyBtn = <HTMLElement>this;
+    console.log("The buy button that was clicked")
+    console.log(currBuyBtn);
+
+    let currProdDiv = currBuyBtn.parentElement;
+    console.log("The parent product div")
+    console.log(currProdDiv);
+
+
+    let prod = new Product();
+    
+    prod.title = currProdDiv.querySelector("div.title").innerHTML;
+
+    prod.price = parseFloat((currProdDiv.querySelector("div.price").innerHTML).replace("$", ""));
+    //let price = currProdDiv.querySelector("div.price").innerHTML;
+    //price = price.replace("$", "");
+    //prod.price = parseFloat(price);
+
+    prod.description = currProdDiv.querySelector("div.description").innerHTML;
+
+    console.log(prod);
 }
